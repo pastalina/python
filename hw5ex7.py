75d5c0b48647dbe2b880b0aceba5cbc1ccbf1d49
pastalina <pastalina@yandex.ru>
2021-05-07 23:36:38 +0300

    homework#5 done

--- hw5ex7.py
+++ hw5ex7.py
@@ -18,18 +18,3 @@ print(firms_profit_list)
 
 with open("hw5ex7.json", "w") as file:
     json.dump(firms_profit_list, file)
-
-
-
-
-
-
-
-
-
-    #     profit = int(firms_list[2]) - int(firms_list[3])
-    #     print(f"Прибыль фирмы {firms_list[0]} составляет: ", profit)
-    #     if profit > 0:
-    #         sum_profit += profit
-    #         c += 1
-    # print("Средняя прибыль составляет: ", sum_profit / c)
